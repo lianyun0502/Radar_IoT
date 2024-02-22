46960acbca620936acf35d5a4864aa6c7d260ce3
Eric Li <lee64690502@gmail.com>
2024-02-22 13:43:26 +0800

    feat: fix read write device register handler bug

--- IoT_Device/Client.py
+++ IoT_Device/Client.py
@@ -14,6 +14,8 @@ class DeviceClient:
         self.integration = KKTIntegration(KKTVComPortConnection(timeout=1))
         self.integration.connectDevice()
 
+        print(self.integration.getChipID()[1])
+
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.mqttc.on_connect = self.on_connect
         self.mqttc.on_message = self.on_message
@@ -26,38 +28,40 @@ class DeviceClient:
         self.post_proc = PostProcess()
 
     def __del__(self):
+        self.mqttc.loop_stop()
         self.mqttc.disconnect()
         self.integration.disconnectDevice()
 
     def run(self):
         self.mqttc.loop_start()
+        
 
     def on_connect(self, client:mqtt.Client, userdata, flags, reason_code, properties):
         print(f"Connected with result code {reason_code}")
         client.subscribe("$SYS/#")
-        client.subscribe("$K{self.device_id}/read_register")
-        client.subscribe("$K{self.device_id}/write_register")
-        client.subscribe("$K{self.device_id}/launch")
-        client.subscribe("$K{self.device_id}/stop")
-        # client.subscribe("$K{self.device_id}/gesture")
+        client.subscribe(f"$K{self.device_id}/read_register")
+        client.subscribe(f"$K{self.device_id}/write_register")
+        client.subscribe(f"$K{self.device_id}/launch")
+        client.subscribe(f"$K{self.device_id}/stop")
+        client.subscribe(f"$K{self.device_id}/gesture")
 
 
     def on_message(self, client:mqtt.Client, userdata, msg:mqtt.MQTTMessage):
-
         if msg.topic == f"$K{self.device_id}/read_register":
             print(f"Read Register: {msg.payload}")
-            self.read_register(msg.payload)
+            self.read_register(msg)
         elif msg.topic == f"$K{self.device_id}/write_register":
             print(f"Write Register: {msg.payload}")
-            self.write_register(msg.payload[:4], msg.payload[4:])
+            self.write_register(msg)
         elif msg.topic == f"$K{self.device_id}/launch":
             print(f"Launch")
             self.launch()
         elif msg.topic == f"$K{self.device_id}/stop":
             print(f"Stop")
             self.stop()
-        elif msg.topic == "$K{self.device_id}/gesture":
+        elif msg.topic == f"$K{self.device_id}/gesture":
             print(f"Gesture: {msg.payload.decode()}")
+            print(f"{time.asctime( time.localtime(msg.timestamp) )}")
 
         # print(msg.topic+" "+str(msg.payload))
 
@@ -70,16 +74,18 @@ class DeviceClient:
         ges = self.post_proc.run(prediction)
         if ges != 0:
             print(ges)
-            self.mqttc.publish("$K{self.device_id}/gesture", str(ges).encode())
+            self.mqttc.publish(f"$K{self.device_id}/gesture", str(ges).encode())
 
-    def read_register(self, address:bytes):
-        address = int.from_bytes(address, "big")
+    def read_register(self, msg:mqtt.MQTTMessage):
+        address = int(msg.payload)
         read = self.integration.readHWRegister(address)
-        print(f'read reg ({hex(address)}) : {read[0]} {hex(read[1])}')
-
-    def write_register(self, address:bytes, value:bytes):
-        address = int.from_bytes(address, "big")
-        value = int.from_bytes(value, "big")
+        if read[0].value == 0:
+            print(f'read value ({hex(address)}) : {hex(read[1])}')
+  
+    def write_register(self, msg:mqtt.MQTTMessage):
+        addr, val = msg.payload[:4], msg.payload[4:]
+        address = int(addr)
+        value = int(val)
         print(f'write reg ({hex(address)}) : {self.integration.writeHWRegister(address, value)}')
 
     def launch(self):
@@ -97,7 +103,7 @@ class DeviceClient:
         self.engine.start()
         
     def stop(self):
-       integration.switchCollectionOfMultiResults(actions=0b0, read_interrupt=0, clear_interrupt=0, raw_size=(8192 + 2) * 2,
+       self.integration.switchCollectionOfMultiResults(actions=0b0, read_interrupt=0, clear_interrupt=0, raw_size=(8192 + 2) * 2,
                                                   ch_of_RBank=1, reg_address=[])
        
 if __name__ == '__main__':
@@ -105,4 +111,5 @@ if __name__ == '__main__':
     device.run()
     device.launch()
     key = input("Press Esc to quit...")
+    # device.mqttc.publish(f"$K{device.device_id}/read_register", str(0x50000504).encode())
     device.stop()
